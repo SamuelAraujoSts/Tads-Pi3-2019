8c112ad91fff9bb45c2cfee0b4c6706b5bb23a68
gabrielle.csilva11 <gabrielle.csilva11@CAS511925W1033.senacsp.edu.br>
2019-08-20 21:46:45 -0300

    Inicio do Controller

--- gerenciador-produtos/src/main/java/br/tads/scorpions/controller/ProdutoController.java
+++ gerenciador-produtos/src/main/java/br/tads/scorpions/controller/ProdutoController.java
@@ -5,10 +5,49 @@
  */
 package br.tads.scorpions.controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author samuel.asantos8
  */
 public class ProdutoController {
+    public static boolean salvar(String pNome, String pDscProduto, Double pPrecCompra,  
+             Double pPrecVenda, int pQuant, boolean pStatus, Date pDataProd)   throws SQLException, Exception {
+        
+        Produto p = new Produto(pNome, pDscProduto, pPrecCompra, pPrecVenda, pQuant, pStatus, pDataProd);
+
+        return ProdutoDAO.inserir(p);
+    }
+
     
+    public static boolean atualizar(int pCodProd, String pNome, String pDscProduto, Double pPrecCompra,  
+             Double pPrecVenda, int pQuant, boolean pStatus, Date pDataProd)   throws SQLException, Exception {
+        
+        Produto p = new Produto(pCodProd, pNome, pDscProduto, pPrecCompra, pPrecVenda, pQuant, pStatus, pDataProd);
+
+        return ProdutoDAO.atualizar(p);
+    }
+    
+    
+    
+    public static boolean excluir(int pCodProd)   throws SQLException, Exception {
+        return ProdutoDAO.excluir(pCodProd);
+    }
+
+    
+    public static ArrayList<Produto> getPesquisa(int id)   
+            throws SQLException, Exception {
+        ArrayList<Produto> produtos = ProdutoDAO.procurar(id);
+       
+        return produtos;
+    }
+    
+    public static Produto getPesquisaID(int id)   throws SQLException, Exception {
+        Produto produtos = ProdutoDAO.procurarID(id);
+       
+        return produtos;
+    }
 }
